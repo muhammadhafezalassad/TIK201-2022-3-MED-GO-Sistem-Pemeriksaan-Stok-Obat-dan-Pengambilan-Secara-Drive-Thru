bfb23d01ad362cfdf2bfd44f8d2faa9572d86ea4
Muhammad Hafez Al - Assad <m.hafez@mhs.unsyiah.ac.id>
2022-04-17 14:44:04 +0700

    Flutter Project

---
+++ med_go/lib/shared/theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color kPrimeColor = Color(0xff5DAAB0);
+const Color kBlueColor = Color(0xff3F31CD);
+const Color kBlackColor = Color(0xff000000);
+const Color kWhiteColor = Color(0xffFFFFFF);
+
+TextStyle blackStyle = GoogleFonts.montserrat(
+  color: kBlackColor,
+);
+TextStyle whiteStyle = GoogleFonts.montserrat(
+  color: kWhiteColor,
+);
+TextStyle greenStyle = GoogleFonts.montserrat(
+  color: kPrimeColor,
+);
+  
