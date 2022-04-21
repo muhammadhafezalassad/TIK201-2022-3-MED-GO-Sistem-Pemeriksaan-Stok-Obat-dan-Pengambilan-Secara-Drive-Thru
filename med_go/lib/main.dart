e9760ebdfc36e40bc12cd76dbdd15080ef17efa9
Muhammad Hafez Al - Assad <m.hafez@mhs.unsyiah.ac.id>
2022-04-21 14:45:11 +0700

    MED-GO
    Fix Debug

--- med_go/lib/main.dart
+++ med_go/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:med_go/ui/pages/get_started_page.dart';
 import 'package:med_go/ui/pages/home_page.dart';
+import 'package:med_go/ui/pages/order_page.dart';
 import 'package:med_go/ui/pages/signin_page.dart';
 import 'package:med_go/ui/pages/signup_page.dart';
 
@@ -13,12 +14,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
+    return MaterialApp(debugShowCheckedModeBanner: false,
       routes: {
         '/': (context) => const GetStartedPage(),
         '/signin': (context) => const SignInPage(),
         '/signup': (context) => const SignUpPage(),
         '/home': (context) => const HomePage(),
+        '/order':(context) => const OrderPage()
       },
     );}}
--- med_go/lib/ui/pages/order_page.dart
+++ med_go/lib/ui/pages/order_page.dart
@@ -1,122 +1,116 @@
 import 'package:flutter/material.dart';
-import 'package:med_go/shared/theme.dart';
+import 'package:med_go/ui/pages/home_page.dart';
 
 class OrderPage extends StatelessWidget {
   const OrderPage({Key? key}) : super(key: key);
-  @override
+ @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Order'),
-        backgroundColor: kPrimeColor,
-      ),
-      body: new Container(
-        child: new Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              new Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    new Text(
-                      "Lokasi Apotek",
-                      style: new TextStyle(
-                          fontSize: 20.0,
-                          color: const Color(0xFF000000),
-                          fontWeight: FontWeight.w800,
-                          fontFamily: "Roboto"),
-                    ),
-                    new Text(
-                      "Filter",
-                      style: new TextStyle(
-                          fontSize: 17.0,
-                          color: const Color(0xFF5DABB0),
-                          fontWeight: FontWeight.w800,
-                          fontFamily: "Roboto"),
-                    )
-                  ]),
-              new Container(
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    //fillColor: Color(0xffF1F0F5),
-                    //filled: true,
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                      borderSide:
-                          BorderSide(color: Color(0xff989898), width: 1.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                      borderSide:
-                          BorderSide(color: Color(0xff989898), width: 1.0),
-                    ),
-                    labelText: 'Search',
-                    labelStyle: TextStyle(
-                        color: Color(0xff989898),
-                        fontFamily: 'PoppinsRegular',
-                        fontSize: 20),
-                  ),
-                ),
-                padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
-                alignment: Alignment.center,
-              ),
-              new Container(
-                child: new Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.all(15),
-                        child: Text(
-                          'Apotek Kopelma Darussalam',
-                          style: TextStyle(
-                            fontSize: 17,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(15),
-                        child: Text(
-                          'Apotek Lingke',
-                          style: TextStyle(
-                            fontSize: 17,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(15),
-                        child: Text(
-                          'Apotek Kajhu',
-                          style: TextStyle(
-                            fontSize: 17,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(15),
-                        child: Text(
-                          'Apotek Batoh',
-                          style: TextStyle(
-                            fontSize: 17,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ),
-                    ]),
-                padding: const EdgeInsets.all(20.0),
-                alignment: Alignment.center,
-              )
-            ]),
-        padding: const EdgeInsets.all(25.0),
-        alignment: Alignment.center,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'Lokasi Apotek',
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: Color(0xFF5DABB0),
+          automaticallyImplyLeading: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+            },
+          ),
+        ),
+        body: ListView(
+          children: [
+            ListApotek(
+              gambar:
+                  './assets/banda.jpg',
+              nama: 'Apotek Kimia Farma',
+              deskripsi: 'Darussalam, Banda Aceh',
+            ),
+            ListApotek(
+              gambar:
+                  './assets/kimia.jpg',
+              nama: 'Apotek Banda Farma',
+              deskripsi: 'Baiturahman, Banda Aceh',
+            ),
+            ListApotek(
+              gambar:
+                  './assets/sehat.jpg',
+              nama: 'Apotek Cinta Sehat',
+              deskripsi: 'Baiturahman, Banda Aceh',
+            ),
+            ListApotek(
+              gambar:
+                  './assets/laris.jpg',
+              nama: 'Apotek Laris 2',
+              deskripsi: 'Peunayong, Banda Aceh',
+            ),
+            ListApotek(
+              gambar:
+                  './assets/rakan.jpg',
+              nama: 'Apotek Rakan Medical',
+              deskripsi: 'Kuta Alam, Banda Aceh',
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class ListApotek extends StatelessWidget {
+  ListApotek(
+      {required this.gambar, required this.nama, required this.deskripsi});
+
+  final String gambar;
+  final String nama;
+  final String deskripsi;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(20),
+        child: InkWell(
+          splashColor: Colors.blue.withAlpha(30),
+          child: Card(
+            child: Row(
+              children: [
+                Image(
+                  image: AssetImage(gambar),
+                  width: 110,
+                  height: 110,
+                  fit: BoxFit.cover,
+                ),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          nama,
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.left,
+                        ),
+                        Text(
+                          deskripsi,
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.left,
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          onTap: () {Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  builder: (BuildContext context) => HomePage()));},
+        ));
   }
 }
